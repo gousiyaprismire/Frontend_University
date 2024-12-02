a8075d861d500d22fd7306213b2117a6ee787438
Balaji <venkatabalajikommi@gmail.com>
2024-12-02 15:30:19 +0530

    Contact

--- src/components/Pages/ContactUs.js
+++ src/components/Pages/ContactUs.js
@@ -3,10 +3,8 @@ import React from 'react';
 const ContactUs = () => {
   return (
     <div>
-      <h1>Contact Us</h1>
-      <p>Hiii</p>
-      <h2>Verification</h2>
-      <h3>Helloo</h3>
+      <h1>Contact Us..</h1>
+      
 
     </div>
   );

4fb4da08b368de54dc37165df900e4163606c310
Balaji <venkatabalajikommi@gmail.com>
2024-12-02 17:46:36 +0530

    ContactUs

--- src/components/Pages/ContactUs.js
+++ src/components/Pages/ContactUs.js
@@ -16,7 +16,7 @@ const ContactUs = () => {
               alt="Yogi Vemana University Map"
               className="map-image"
             /> */}
-            <p className='view-jntu'>JNTU.Kakinada</p>
+            <p className='view-jntu'> JNTU.Kakinada</p>
             <a
               href="https://www.google.com/maps/dir//Jawaharlal+Nehru+Technological+University,+Kakinada,+Andhra+Pradesh+533003/@16.9796435,82.2350907,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3a38299cdd8f2049:0xb7251fd704659869!2m2!1d82.242755!2d16.9782157?entry=ttu"
               target="_blank"

36ad9605b60a8c79b2e965d870a3abd7553634e5
Balaji <venkatabalajikommi@gmail.com>
2024-12-02 18:11:58 +0530

    Contact

--- src/components/Pages/ContactUs.js
+++ src/components/Pages/ContactUs.js
@@ -8,7 +8,7 @@ const ContactUs = () => {
         <div className="card map-card">
           <h4>Contact Us</h4>
           <div className="line"></div>
-          <h3>Keep In TOuch</h3>
+          <h3>Keep In Touch</h3>
 
           <div className="map-image-wrapper">
             {/* <img
@@ -23,7 +23,7 @@ const ContactUs = () => {
               rel="noopener noreferrer"
               className="view-large-map-link"
             >
-              View on Large   Map
+              View on Large  Map
             </a>
           </div>
         </div>
--- src/components/Pages/VerificationScreen.js
+++ src/components/Pages/VerificationScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VerificationScreen = () => {
   return (
     <div>
-      <h1>This is the Verification Page</h1>
+      <h1>This is the Verification  Page</h1>
     </div>
   );
 };

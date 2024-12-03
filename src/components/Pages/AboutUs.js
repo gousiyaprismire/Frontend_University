614b00fc89abd33def07d59de2d699020505ad89
RamyaBonu <ramyabonu104@gmail.com>
2024-12-03 17:42:26 +0530

    ramyaAbout

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -30,7 +30,7 @@ const AboutUs = () => {
         }
 
         .card {
-          border: 1.1px solid #ddd;
+          border: 1px solid #ddd;
           border-radius: 10px;
           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
           transition: transform 0.2s;

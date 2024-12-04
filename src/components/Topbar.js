7d84ee2941248245542507b2397e0c7e050b26f7
tkpriya13 <keerthi.lanka13@gmail.com>
2024-12-04 17:50:04 +0530

    Homepage,layoutchanges

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,9 +3,19 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Topbar = () => {
   return (
-    <AppBar position="fixed" sx={{ zIndex: 1201, bgcolor: '#555' }}>
+    <AppBar
+      position="fixed"
+      sx={{
+        zIndex: 1201,
+        
+        bgcolor: '#555',
+        height: '64px',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       <Toolbar>
-        <Typography variant="h6" noWrap>
+        <Typography variant="h6" noWrap component="div">
           University App
         </Typography>
       </Toolbar>

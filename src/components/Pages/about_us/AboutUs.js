35ad75567d9b3c80f3d5353119a187dc9f751a5b
RamyaBonu <ramyabonu104@gmail.com>
2024-12-05 18:25:13 +0530

    about

--- src/components/Pages/about_us/AboutUs.js
+++ src/components/Pages/about_us/AboutUs.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './AboutUs.css'; 
 
+
 const AboutUs = () => {
   return (
     <>
       <h1>About Us</h1>
-      <img
+      <img className='img1'
         src="https://jcpatriot.com/wp-content/uploads/2016/04/REAL-university-of-florida-pic.jpg"
         title="Prismire University"
         alt="university pic"
+        style={{ width: '100%', height: '500px' }} 
       />
       <p>
-        Prismire University is a distinguished institution dedicated to academic excellence, innovation, and community
+        UK University is a distinguished institution dedicated to academic excellence, innovation, and community
         engagement. Offering a wide range of undergraduate, graduate, and professional programs, it provides students with
         the knowledge and skills necessary for success in a dynamic world. The university is renowned for its world-class
         faculty, cutting-edge research opportunities, and modern facilities. With a focus on critical thinking, hands-on
@@ -19,47 +21,52 @@ const AboutUs = () => {
         university promotes a vibrant campus life through diverse student organizations, extracurricular activities, and cultural
         events, fostering a well-rounded educational experience.
       </p>
-
-      <div>
+      
+      
+      <div style={{ backgroundColor: '#E0F7FA', padding: '20px' }}>
         <h2>Our Goals</h2>
-
         <div className="row row-cols-1 row-cols-md-3 g-4">
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
-                src="https://image.freepik.com/free-vector/academic-excellence-illustration_24877-52359.jpg"
+                src="https://www.shutterstock.com/image-vector/realistic-university-building-graduate-cap-600nw-2485116275.jpg"
                 className="card-img-top"
                 alt="pic1"
+                style={{ height: '150px', objectFit: 'fit' }} 
               />
               <div className="card-body">
                 <h5 className="card-title">Academic Excellence</h5>
                 <p className="card-text">
-                  Providing high-quality education that equips students with the knowledge, skills, and critical thinking
-                  abilities necessary for personal and professional success.
+                Providing high-quality education that empowers students with the knowledge, skills, and critical 
+                thinking needed for personal development and professional success in a dynamic world.
                 </p>
               </div>
             </div>
           </div>
 
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
                 src="https://corporate.travelclinic.com/wp-content/uploads/2020/07/Services-Innovation-and-research-2048x1367.jpg"
                 className="card-img-top"
                 alt="pic2"
+                style={{ height: '150px', objectFit: 'cover' }}
               />
               <div className="card-body">
                 <h5 className="card-title">Research and Innovation</h5>
-                <p className="card-text">Advancing knowledge through research, encouraging creativity, and developing solutions to global challenges.</p>
+                <p className="card-text">
+                Research and innovation in universities foster knowledge growth, creativity, and solutions to global challenges, advancing academics and societal development.</p>
               </div>
             </div>
           </div>
+
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
                 src="https://agribusinessedu.com/wp-content/uploads/2020/12/social-responsibility-and-ethics-for-agribusiness-2020-12-06-450545.jpg"
                 className="card-img-top"
                 alt="pic3"
+                style={{ height: '150px', objectFit: 'cover' }}
               />
               <div className="card-body">
                 <h5 className="card-title">Social and Ethical Responsibility</h5>
@@ -69,12 +76,14 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
+
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
                 src="https://centerforhci.org/wp-content/uploads/2022/06/What-are-the-Benefits-of-Career-Planning-1.jpg"
                 className="card-img-top"
                 alt="pic4"
+                style={{ height: '150px', objectFit: 'cover' }}
               />
               <div className="card-body">
                 <h5 className="card-title">Career Preparation</h5>
@@ -84,12 +93,14 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
+
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
                 src="https://d3timt52sxdbq0.cloudfront.net/wp-content/uploads/2016/04/benefitsofcollaboration.jpg"
                 className="card-img-top"
                 alt="pic5"
+                style={{ height: '150px', objectFit: 'cover' }}
               />
               <div className="card-body">
                 <h5 className="card-title">Collaboration and Partnership</h5>
@@ -99,105 +110,96 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
+
           <div className="col">
-            <div className="card h-100">
+            <div className="card h-auto" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
               <img
                 src="https://www.continents.us/wp-content/uploads/2023/11/why-is-higher-education-important-to-you-personally-and-professionally-1-600x600.jpg"
                 className="card-img-top"
                 alt="pic6"
+                style={{ height: '150px', objectFit: 'cover' }}
               />
               <div className="card-body">
                 <h5 className="card-title">Personal Development</h5>
                 <p className="card-text">
-                  University goals focus on academic excellence, research, career preparation, social responsibility, global perspectives, personal development, leadership, and emotional intelligence.
+                 It emphasizes enhancing students' self-awareness, emotional intelligence, leadership skills, and overall growth for future success. 
                 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+     </div>
+
+    
+     <div style={{ backgroundColor: '#FFF8DC', padding: '20px'}}>
+        <h2>Members of the University</h2>
+        <div className="container" style={{ marginTop: '0px' }}>
+          <div className="row row-cols-1 row-cols-md-4 g-4">
+            <div className="col">
+              <div className="card" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
+                <img
+                  src="https://www.nesl.edu/images/default-source/admissions-aid-hero-images/5ef7f25a-a960-47c1-9d70-1a203a6c3ad4.tmb-wide.jpg?Status=Master&Culture=en&sfvrsn=5ba07fa0_1"
+                  className="card-img-top"
+                  alt="Steven Pinker"
+                  style={{ height: '200px', objectFit: 'fit', borderRadius: '50%' }}
+                />
+                <pre>
+                  <h5 className="card-title">    STEVEN PINKER</h5>
+                  <h5 className="card-title">     Chancellor</h5>
+                </pre>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
+                <img
+                  src="https://www.law.columbia.edu/sites/default/files/styles/260x312/public/2024-11/madhav.0435cc_crop.jpg?h=7f3af489&itok=6HL2hRVM"
+                  className="card-img-top"
+                  alt="ALBERT"
+                  style={{ height: '200px', objectFit: 'fit', borderRadius: '50%' }}
+                />
+                <pre>
+                  <h5 className="card-title">         ALBERT</h5>
+                  <h5 className="card-title">    Vice Chancellor</h5>
+                </pre>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj2Zy7VNCVjz8yQ7hUvl-dvmy4n9FURfl33SxqLEbpRqr786QI8lYyO2wtGnUW0m5vwSk&usqp=CAU"
+                  className="card-img-top"
+                  alt="EDWARD"
+                  style={{ height: '200px', objectFit: 'fit', borderRadius: '50%' }}
+                />
+                <pre>
+                  <h5 className="card-title">         EDWARD</h5>
+                  <h5 className="card-title">     Payroll Manager</h5>
+                </pre>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card" style={{ width: '100%', maxWidth: '350px', margin: '0 auto' }}>
+                <img
+                  src="https://tse4.mm.bing.net/th?id=OIP.Kxh2Bu1CQFPBZ_MEYYYjqwHaJQ&pid=Api&P=0&h=180"
+                  className="card-img-top"
+                  alt="LUSY"
+                  style={{ height: '200px', objectFit: 'fit', borderRadius: '50%' }}
+                />
+                <pre>
+                  <h5 className="card-title">           LUSY</h5>
+                  <h5 className="card-title">    Dean of Academics</h5>
+                </pre>
               </div>
             </div>
           </div>
         </div>
       </div>
 
+    </>
+  );
+};
 
-    <h2>Members of the university</h2>
-    
-    <div className="container mt-5">
-      <div className="row row-cols-1 row-cols-md-4 g-4">
-        <div className="col">
-          <div className="card">
-            <img
-              src="https://medschool.umich.edu/sites/default/files/styles/rounded_1_1/public/2023-06/johnforen.jpg?h=6133d959&itok=g4dd2UVF"
-              className="card-img-top"
-              alt="Steven Pinker"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Steven Pinker</h5>
-              <p className="card-text">Chancellor</p>
-            </div>
-          </div>
-        </div>
-
-  
-        <div className="col">
-          <div className="card">
-            <img
-              src="https://www.nottingham.ac.uk/News/images/ColinCampbell1.jpg"
-              className="card-img-top"
-              alt="ALBERT"
-            />
-            <div className="card-body">
-              <h5 className="card-title">ALBERT</h5>
-              <p className="card-text">Vice Chancellor</p>
-             
-            </div>
-          </div>
-        </div>
-
-       
-        <div className="col">
-          <div className="card">
-            <img
-              src="https://c8.alamy.com/comp/W0FN3B/james-schlessinger-professor-at-stanford-university-testifies-before-the-senate-foreign-relations-committee-regarding-us-russian-arms-control-issues-on-capitol-hill-in-washington-on-april-29-2010-upiroger-l-wollenberg-W0FN3B.jpg"
-              className="card-img-top"
-              alt="EDWARD"
-            />
-            <div className="card-body">
-              <h5 className="card-title">EDWARD</h5>
-              <p className="card-text">Payroll Manager</p>
-             
-            </div>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="card">
-            <img
-              src="https://tse4.mm.bing.net/th?id=OIP.Kxh2Bu1CQFPBZ_MEYYYjqwHaJQ&pid=Api&P=0&h=180"
-              className="card-img-top"
-              alt="LUSY"
-            />
-            <div className="card-body">
-              <h5 className="card-title">LUSY</h5>
-              <p className="card-text">Head of Department</p>
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
- 
-
-
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      </>
-  );};
-
-  export default AboutUs;
-
-  
-   
+export default AboutUs;

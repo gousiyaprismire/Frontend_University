bae8e2fa56ef2efa301896f6223809afe89e4df8
gousiyaprismire <gousiyaprismire@gmail.com>
2024-12-10 12:53:05 +0530

    Admissions Page

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import HallOfFame from './components/Pages/HallOfFame/HallOfFame';
 import RulesAndRegulations from './components/Pages/RulesAndRegulations/RulesAndRegulations';
 import AboutUs from './components/Pages/about_us/AboutUs';
 import ContactUs from './components/Pages/Contactus/ContactUs';
-import Jobs from './components/Pages/Jobs/Jobs'
+import Admissions from './components/Pages/Admissions/Admissions';
 import Study from './components/Pages/studyOptions/Study';
 import Scholarship from './components/Pages/Scholarship/Scholarship';
 import Sports from './components/Pages/Sports/Sports';
@@ -145,7 +145,7 @@ const App = () => {
               <Route path="/sports" element={<Sports />} />
               <Route path="/study" element={<Study />} />
               <Route path="/scholarship" element={<Scholarship />} />
-              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/admissions" element={<Admissions />} />
               <Route path="/news" element={<News />} />
 
               {!loggedIn && (
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -37,7 +37,7 @@ const Sidebar = () => {
     // { key: '/contact-us', icon: <MailOutlined />, label: 'Contact Us' },
     { key: '/study', icon: <ReadOutlined />, label: 'Study' },
     { key: '/scholarship', icon: <BankOutlined />, label: 'Scholarship' },
-    { key: '/jobs', icon: <BankOutlined />, label: 'Jobs' },
+    { key: '/admissions', icon: <ReadOutlined />, label: 'Admissions' },
     { key: '/news', icon: <ReadOutlined />, label: 'News' },
     { key: '/online-courses', icon: <ReadOutlined />, label: 'Online Courses' },
 
